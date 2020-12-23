6febcea57a5df408c3742b9480e091b3eba6cf11
Kate <45261018+katelk@users.noreply.github.com>
2020-12-23 12:34:11 +0300

    Update market.py

--- market.py
+++ market.py
@@ -1,19 +1,26 @@
-def market(values: dict):  # values = {"user_id1": Q1, "user_id2": Q2, "user_id3": Q3)}
+import random
+
+def market(values: dict, a, b, MC):  # values = {"user_id1": Q1, "user_id2": Q2, "user_id3": Q3)}
     players = values.keys()
-    a = 300                 # Qd = a - bP
-    b = 5                   # P = (a - Qd)/b
+    # a = 300                 # Qd = a - bP
+    # b = 5                   # P = (a - Qd)/b
     Q = 0
-    MC = 5
+    # MC = 5
     result = {}
     for player in players:
         Q += values[player]
     if Q >= a:
         for player in players:
-            result[player] = - values[player]*MC  # прибыль
+            result[player] = - round(values[player]*MC, 1)  # прибыль
     else:
         P = (a - Q)/b
         for player in players:
-            result[player] = values[player] * (P - MC)
+            result[player] = round(values[player] * (P - MC), 1)
 
     return result  # result = {"user_id1": income1, "user_id2": income2, "user_id3": income3)}
 
+def set_params():
+    a = random.randint(200, 500)
+    b = random.randint(5, 20)
+    mc = a/b * random.choice([0.2, 0.25, 0.3, 0.35])
+    return (a, b, mc)
